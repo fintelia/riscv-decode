c1b35c3cea3d0dee5f043d9895df51fcc9546c3c
Jonathan Behrens <fintelia@gmail.com>
2019-01-28 16:11:56 -0500

    Add instruction_length function

--- src/lib.rs
+++ src/lib.rs
@@ -58,6 +58,23 @@ pub fn try_decode_system(i: u32) -> Option<Instruction> {
     None
 }
 
+/// Return the length (in bytes) of an instruction given the low 16 bits of it. The current spec
+/// reserves a bit pattern for instructions of length >= 192 bits, but for simplicity this function
+/// just returns 24 in that case.
+pub fn instruction_length(i: u16) -> usize {
+    if i & 0b11 != 0b11 {
+        2
+    } else if i & 0b11100 != 0b11100 {
+        4
+    } else if i & 0b111111 == 0b011111 {
+        6
+    } else if i & 0b1111111 == 0b011111 {
+        8
+    } else {
+        10 + 2 * ((i >> 12) & 0b111) as usize
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
